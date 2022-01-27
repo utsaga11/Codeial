27783e06b5cd6872a7cbc6bccdbc8ba096b73fcb
utsaga11 <utsaga22@gmail.com>
2022-01-27 12:38:19 +0530

    second commit

---
+++ controllers/users_controller.js
@@ -0,0 +1,9 @@
+module.exports.profile = function(req,res){
+    return res.render('profile', {
+        title: "Profile"
+    });
+}
+
+module.exports.posts = function(req,res){
+    return res.end('<h1> User Post </h1>');
+}
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+// static files
+app.use(express.static('./assets'));
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// call expresslayouts 
+app.use(expressLayouts);
+
+// set view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//use express router
+app.use('/', require('./routes'))
 
 app.listen(port, function(err){
     if(err){
