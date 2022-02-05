0c7fe21a83ff1ff17bcaecb6d0c92d8135db930a
utsaga11 <utsaga22@gmail.com>
2022-02-06 00:57:28 +0530

    async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,24 +1,27 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function (req, res){
+module.exports.home = async function (req, res){
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+           path: 'comments',
+           populate:{
+               path:'user'
+           } 
+        });
+         //sending all users
+        let users = await User.find({});
+            return res.render('home', {
+             title: "Codeial || Home",
+             posts: posts,
+             all_users: users 
+         });
+    }catch(err){
+        console.log("Error",err);
+        return;
+    }
     // populating the post with user
-    Post.find({})
-    .populate('user')
-    .populate({
-       path: 'comments',
-       populate:{
-           path:'user'
-       } 
-    })
-    .exec(function (err, posts) {
-        //sending all users
-        User.find({},function(err,users){
-           return res.render('home', {
-            title: "Codeial || Home",
-            posts: posts,
-            all_users: users
-        });  
-        })
-       });
+  
 }
